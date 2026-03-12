d3cf0020cb1f5f64c4bb39e6b8160d07d1a47362
MrHanty1488 <levkolos23@gmail.com>
2026-03-12 08:02:55 +0000

    удалил мусор

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\HanTY\Desktop\cs2 cheat project"
-AnarchyInjector_x64.exe cs2.exe "C:\Users\HanTY\Desktop\cs2 cheat project\cs2_rust_internal\target\release\cs2_rust_internal.dll"
-pause
